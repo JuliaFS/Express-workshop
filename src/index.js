5b30454f06ef6f4a547d94f51d4b2e9d66725a81
Yulia <yuliya.f.s@gmail.com>
2023-09-25 11:53:26 +0300

    added web server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-console.log('Second Hiiii....')
-console.log('can not commit to github')
-console.log('comit status 200 :)))')
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => { //parvi endpoint
+    res.send('Hello from express!');
+}); 
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
