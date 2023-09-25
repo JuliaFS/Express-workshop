1173f9c928d5d7ab603bca3302dd2a3db86bcb68
Yulia <yuliya.f.s@gmail.com>
2023-09-25 12:17:44 +0300

    added handlebars configurations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+
 const app = express();
+
 const PORT = 5000;
 
+//Handlebrs configuration
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('viw engine', 'hbs');
+
+//Routes
 app.get('/', (req, res) => { //parvi endpoint
     res.send('Hello from express!');
 }); 
